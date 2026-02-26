d9812d5a0d661921882ca3943b954cadd5e1aff3
Sujal Shrestha <sujal84071@gmail.com>
2026-02-26 14:13:36 +0545

    fixing backend push

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: [
+    process.env.CLIENT_URL,
+  ],
   credentials: true,
 }));
 app.use(express.json());
